c45db8d7c75e095b4658b32eebb0b021bdd414ec
Saíd Ahmed <ssaidad7@gmail.com>
2018-10-24 14:52:31 -0400

    added a splash screen

---
+++ WAVES/WAVES/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WAVES
+//
+//  Created by Said Ahmed on 10/13/18.
+//  Copyright © 2018 WAVES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
